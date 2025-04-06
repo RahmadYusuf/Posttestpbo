1606a9063a420a52f7fc5fbb87d711a861f94075
RahmadYusuf <162000552+RahmadYusuf@users.noreply.github.com>
2025-04-06 12:36:18 +0800

    Pengguna.java

--- posttest3/src/model/Pengguna.java
+++ posttest3/src/model/Pengguna.java
@@ -1,11 +1,7 @@
 package model;
 
-
 public class Pengguna {
-    private String nama;
-    private String username;
-    private String password;
-    protected String role;
+    protected String nama, username, password, role;
 
     public Pengguna(String nama, String username, String password, String role) {
         this.nama = nama;
@@ -14,79 +10,17 @@ public class Pengguna {
         this.role = role;
     }
 
-    public String getNama() {
-        return nama;
-    }
+    public String getNama() { 
+        return nama; }
 
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        if (role.equalsIgnoreCase("admin") || role.equalsIgnoreCase("karyawan")) {
-            this.role = role.toLowerCase();
-        } else {
-            throw new IllegalArgumentException("Peran tidak valid! Harus 'admin' atau 'karyawan'.");
-        }
-    }
+    public String getRole() { 
+        return role; }
+    
+    public void setRole(String role) { 
+        this.role = role; }
 
     public void tampilkanInfo() {
         System.out.println("Nama: " + nama);
-        System.out.println("Username: " + username);
         System.out.println("Peran: " + role);
-    }
-}
-
-class Admin extends Pengguna {
-
-    public Admin(String nama, String username, String password) {
-        super(nama, username, password, "admin"); 
-    }
-
-    public void manageUsers() {
-        System.out.println("Admin dapat mengelola pengguna lainnya.");
-    }
-
-    @Override
-    public void tampilkanInfo() {
-        super.tampilkanInfo();
-        System.out.println("Ini adalah akun Admin.");
-    }
-}
-
-class Karyawan extends Pengguna {
-
-    public Karyawan(String nama, String username, String password) {
-        super(nama, username, password, "karyawan"); 
-    }
-
-    public void viewTasks() {
-        System.out.println("Karyawan dapat melihat dan mengerjakan tugas.");
-    }
-
-    @Override
-    public void tampilkanInfo() {
-        super.tampilkanInfo();
-        System.out.println("Ini adalah akun Karyawan.");
     }
 }
