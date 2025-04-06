c469805df6a2bee9b19d6b2ed5f2bcf7c8b07128
RahmadYusuf <162000552+RahmadYusuf@users.noreply.github.com>
2025-04-06 12:37:21 +0800

    Karyawan.java

--- posttest3/src/model/Karyawan.java
+++ posttest3/src/model/Karyawan.java
@@ -1,1 +1,12 @@
+package model;
 
+public class Karyawan extends Pengguna {
+    public Karyawan(String nama, String username, String password) {
+        super(nama, username, password, "karyawan");
+    }
+
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Ini adalah akun Karyawan.");
+    }
+}
