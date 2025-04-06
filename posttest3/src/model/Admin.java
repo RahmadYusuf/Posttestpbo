c1ca2c506cf6a1652652f1e7aa8c256bd655f310
RahmadYusuf <162000552+RahmadYusuf@users.noreply.github.com>
2025-04-06 12:34:51 +0800

    Update Admin.java

--- posttest3/src/model/Admin.java
+++ posttest3/src/model/Admin.java
@@ -1,1 +1,13 @@
+package model;
+
+public class Admin extends Pengguna {
+    public Admin(String nama, String username, String password) {
+        super(nama, username, password, "admin");
+    }
+
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Ini adalah akun Admin.");
+    }
+}
 
