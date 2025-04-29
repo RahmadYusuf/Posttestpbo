d0fe475c2918106dfb582cd555e634a7b984d2b6
RahmadYusuf <162000552+RahmadYusuf@users.noreply.github.com>
2025-04-29 14:13:10 +0800

    Add files via upload

---
+++ Posttest6/src/model/Admin.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Admin extends Pengguna {
+    public Admin(String nama, String username, String password) {
+        super(nama, username, password, "admin");
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Ini adalah akun Admin.");
+    }
+
+    @Override
+    public void hakAkses() {
+        System.out.println("Hak akses penuh ke semua fitur.");
+    }
+}
